e985a59ee92e081be51d1e8f03b966d0a7d09a1d
murielchen100 <127270571+murielchen100@users.noreply.github.com>
2025-09-03 21:25:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -46,6 +46,10 @@ if diamond_file and package_file:
 
     st.write("分配結果：")
     for res in results:
-        st.write(f"分包{res['分包編號']}：{res['分配鑽石']}，總重：{res['總重']}")
+        if isinstance(res['總重'], float):
+    st.write(f"分包{res['分包編號']}：{res['分配鑽石']}，總重：{res['總重']:.3f}")
+else:
+    st.write(f"分包{res['分包編號']}：{res['分配鑽石']}，總重：{res['總重']}")
+
 
 
